108fd9f08cd92cd540679ec95fbdc42f951a5e38
Tobias Knorr <tkh12@web.de>
2019-11-13 10:51:34 +0100

    changed some code in boaaa

--- tools/boaaa/boaaa.cpp
+++ tools/boaaa/boaaa.cpp
@@ -2,15 +2,57 @@
 #include "llvm/IRReader/IRReader.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/Analysis/BasicAliasAnalysis.h"
-#include 
+#include "llvm/"
 
 void main() {
 	
-	llvm::SMDiagnostic smd;
-	llvm::LLVMContext& context = new llvm::LLVMContext();
-	std::unique_ptr<llvm::Module> module = llvm::getLazyIRFileModule(llvm::StringRef::StringRef("test.bc"), smd, context, false);
-	llvm::AAManager manager;
-	manager.registerFunctionAnalysis<llvm::BasicAA>();
-	llvm::FunctionAnalysisManager fam = new llvm::FunctionAnalysisManager(true);
-	llvm::AAResults res = manager.run(module->getFunctionList().front(), fam);
+	LLVMContext Context;
+
+	InitializeAllTargets();
+	InitializeAllTargetMCs();
+	InitializeAllAsmPrinters();
+	InitializeAllAsmParsers();
+
+	// Initialize passes
+	PassRegistry& Registry = *PassRegistry::getPassRegistry();
+	initializeCore(Registry);
+	initializeCoroutines(Registry);
+	initializeScalarOpts(Registry);
+	initializeObjCARCOpts(Registry);
+	initializeVectorization(Registry);
+	initializeIPO(Registry);
+	initializeAnalysis(Registry);
+	initializeTransformUtils(Registry);
+	initializeInstCombine(Registry);
+	initializeAggressiveInstCombine(Registry);
+	initializeInstrumentation(Registry);
+	initializeTarget(Registry);
+	// For codegen passes, only passes that do IR to IR transformation are
+	// supported.
+	/*
+	initializeExpandMemCmpPassPass(Registry);
+	initializeScalarizeMaskedMemIntrinPass(Registry);
+	initializeCodeGenPreparePass(Registry);
+	initializeAtomicExpandPass(Registry);
+	initializeRewriteSymbolsLegacyPassPass(Registry);
+	initializeWinEHPreparePass(Registry);
+	initializeDwarfEHPreparePass(Registry);
+	initializeSafeStackLegacyPassPass(Registry);
+	initializeSjLjEHPreparePass(Registry);
+	initializeStackProtectorPass(Registry);
+	initializePreISelIntrinsicLoweringLegacyPassPass(Registry);
+	initializeGlobalMergePass(Registry);
+	initializeIndirectBrExpandPassPass(Registry);
+	initializeInterleavedLoadCombinePass(Registry);
+	initializeInterleavedAccessPass(Registry);
+	initializeEntryExitInstrumenterPass(Registry);
+	initializePostInlineEntryExitInstrumenterPass(Registry);
+	initializeUnreachableBlockElimLegacyPassPass(Registry);
+	initializeExpandReductionsPass(Registry);
+	initializeWasmEHPreparePass(Registry);
+	initializeWriteBitcodePassPass(Registry);
+	initializeHardwareLoopsPass(Registry);
+	*/
+
+	SMDiagnostic Err;
 }
