40c70012e42277402260b60c041105f8b45f78e7
Tobias Knorr <tkh12@web.de>
2020-04-28 13:22:58 +0200

    fixt issue with gcc and clang, when passing no or only macro argument to BOAAA_CONSUME

--- include/boaaa/support/consume_macro.h
+++ include/boaaa/support/consume_macro.h
@@ -3,13 +3,13 @@
 
 /*
  * defines BOAAA_CONSUME(macro, __VA_ARGS__)
- * 
- * this macro expands for each variadic argument 1 time to macro(arg) for example: 
+ *
+ * this macro expands for each variadic argument 1 time to macro(arg) for example:
  *
  * BOAAA_CONSUME(macro, 1, 2, 3, 4, 5) -> macro(1) macro(2) macro(3) macro(5) macro(6)
- * 
+ *
  * the macro gets evaluated. Following example was tested while writing this macro.
- * 
+ *
  * ===========================================================================
  * #include <iostream>
  *
@@ -17,7 +17,7 @@
  *   static int i = 0;
  *   i += x;
  *   return i;
- * } 
+ * }
  *
  * #define macro(x) i = foo(x);
  *
@@ -34,8 +34,7 @@
  *
  * > main returns 15.
  *
- * BOAAA_CONSUME() and BOAAA_CONSUME(macro) will expend to NOTHING
- *
+ * BOAAA_CONSUME() BOAAA_CONSUME(macro) get replaced with nothing, works on all compilers tested compilers
  */
 
 #define NOTHING 
@@ -47,18 +46,19 @@
 #define BOAAA_EVAL_2(a, ...) NOTHING a NOTHING (__VA_ARGS__)
 #define BOAAA_EVAL_3(f, ...) NOTHING BOAAA_EVAL(f(__VA_ARGS__))
 
-// 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50,
+ // 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50,
 #define BOAAA_SEQ_49_0()      49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10,  9,  8,  7,  6,  5,  4,  3,  2,  1,  0
 //  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,
 #define B0AAA_SEQ_49x1_1x0()   1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0
 //_50,_51,_52,_53,_54,_55,_56,_57,_58,_59,_60,_61,_62,_63,_64,_65,_66,_67,_68,_69,_70,_71,_72,_73,_74,_75,_76,_77,_78,_79,_80,_81,_82,_83,_84,_85,_86,_87,_88,_89,_90,_91,_92,_93,_94,_95,_96,_97,_98,_99,
-#define BOAAA_ARG_N(          _1, _2, _3, _4, _5, _6, _7, _8, _9,_10,_11,_12,_13,_14,_15,_16,_17,_18,_19,_20,_21,_22,_23,_24,_25,_26,_27,_28,_29,_30,_31,_32,_33,_34,_35,_36,_37,_38,_39,_40,_41,_42,_43,_44,_45,_46,_47,_48,_49, N,...) N
+#define BOAAA_ARG_N(         _1, _2, _3, _4, _5, _6, _7, _8, _9,_10,_11,_12,_13,_14,_15,_16,_17,_18,_19,_20,_21,_22,_23,_24,_25,_26,_27,_28,_29,_30,_31,_32,_33,_34,_35,_36,_37,_38,_39,_40,_41,_42,_43,_44,_45,_46,_47,_48,_49, N,...) N
 
 
 #define BOAAA_NARG_(...) BOAAA_ARG_N(__VA_ARGS__)
 //count args -1 because of BOAAA_CALL_0, same with count
-#define BOAAA_COUNT_ARG(...) NOTHING BOAAA_EVAL(BOAAA_ARG_N(__VA_ARGS__, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10,  9,  8,  7,  6,  5,  4,  3,  2,  1,  0 ))
-#define BOAAA_IS_EMPTY(...)  NOTHING BOAAA_EVAL(BOAAA_ARG_N(__VA_ARGS__,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0 ))
+#define BOAAA_COUNT_ARG(...)            NOTHING BOAAA_EVAL(BOAAA_ARG_N(__VA_ARGS__, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10,  9,  8,  7,  6,  5,  4,  3,  2,  1,  0 ))
+#define BOAAA_IS_EMPTY(...)             NOTHING BOAAA_EVAL(BOAAA_ARG_N(__VA_ARGS__,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0 ))
+#define BOAAA_IS_ZERO_OR_ONE_ARG(...)   NOTHING BOAAA_EVAL(BOAAA_ARG_N(__VA_ARGS__,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0 ))
 
 #define BOAAA_PRIMITIVE_CONCAT(A, ...) A ## __VA_ARGS__
 
@@ -142,7 +142,7 @@
 #define BOAAA_CONSUME_NO_ZERO_ARGS(...)		      NOTHING BOAAA_CONSUME_EVAL_2(__VA_ARGS__)
 #define BOAAA_CONSUME_EXTENDED_2(COND, ...)	      NOTHING BOAAA_EVAL(BOAAA_IFF_ ## COND (__VA_ARGS__))
 #define BOAAA_CONSUME_EXTENDED_1(...)			  NOTHING BOAAA_EVAL(BOAAA_CONSUME_EXTENDED_2(__VA_ARGS__))
-#define BOAAA_IS_EMPTY_MINUS_FUNC(FUNC, ...)      NOTHING BOAAA_IS_EMPTY(__VA_ARGS__)
-#define BOAAA_CONSUME(...)					      NOTHING BOAAA_EVAL(BOAAA_CONSUME_EXTENDED_1(BOAAA_IS_EMPTY_MINUS_FUNC(__VA_ARGS__), BOAAA_CONSUME_NO_ZERO_ARGS(__VA_ARGS__), NOTHING))
+#define BOAAA_IS_EMPTY_MINUS_FUNC(...)            NOTHING BOAAA_IS_ZERO_OR_ONE_ARG(__VA_ARGS__)
+#define BOAAA_CONSUME(...)					      NOTHING BOAAA_EVAL(BOAAA_CONSUME_EXTENDED_1(BOAAA_IS_EMPTY_MINUS_FUNC(__VA_ARGS__), BOAAA_CONSUME_NO_ZERO_ARGS(__VA_ARGS__, 0, 0), NOTHING))
 
 #endif //!BOAAA_CONSUME_MACRO
